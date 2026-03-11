a79254dfa2fe34d306992d4f227f6dcb9b7d4e5e
RohanDSkaria <rohandanny484@gmail.com>
2026-03-11 06:36:09 +0530

    feat: enable line numbers for man pages

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.g.maplocalleader = " "
 
 require("config.options")
 require("config.keymaps")
+require("config.autocmds")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)

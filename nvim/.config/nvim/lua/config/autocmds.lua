a6a8525d0b4bc83197a8a3c957ad611d8406cdf6
RohanDSkaria <rohandanny484@gmail.com>
2026-03-18 06:46:28 +0530

    feat: add autocmd for Go file formatting on save

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.relativenumber = true
     end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        vim.cmd("silent !goimports -w %")
+    end,
+})
